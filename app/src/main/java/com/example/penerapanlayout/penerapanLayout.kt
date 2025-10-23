0f2ffd983dc8f5bd6eadefe58fd5dc9f7290f8f0
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-23 08:25:54 +0700

    feat: membuat function baru untuk membuat tata letak dengan nama 'AktivitasPertama'

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -1,2 +1,9 @@
 package com.example.penerapanlayout
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ActivitisPertama(modifier: Modifier) {
+
+}

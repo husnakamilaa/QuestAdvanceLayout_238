05291a059862371b7c49c0dc96df237484053d57
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-23 08:30:53 +0700

    feat: membuat layout column dengan properties tampilan penuh dan konten didalamnya berada ditengah baris

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -1,9 +1,18 @@
 package com.example.penerapanlayout
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ActivitisPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }

25266243c7975c193ef42f949afb46864491a9f8
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-23 09:13:24 +0700

    feat: menambahkan Box dengan memodifikasi ukuran layar menjadi penuh

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -1,6 +1,7 @@
 package com.example.penerapanlayout
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -66,6 +68,12 @@ fun ActivitisPertama(modifier: Modifier) {
                     )
                 }
             }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+
         }
     }
 }

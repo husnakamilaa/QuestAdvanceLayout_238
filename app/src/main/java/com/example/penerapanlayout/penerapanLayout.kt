2c6005d10917e7b5e00d801806d6064b68f2ab0a
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-23 08:57:44 +0700

    feat: menambahkan variable gambar di dalam card

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,7 +39,7 @@ fun ActivitisPertama(modifier: Modifier) {
                 containerColor = Color.DarkGray
             )) {
             Row() {
-
+                val gambar = painterResource(id = R.drawable.melukis))
             }
         }
     }

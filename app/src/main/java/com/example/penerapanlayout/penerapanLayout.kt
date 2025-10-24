190213df78951750d33619c2081a441ed81dddc2
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-24 17:00:23 +0700

    feat: menambahkan komponen image pada raw pertama beserta modifikasi properties didalmnya, seperti ukuran maupun padding

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -79,6 +79,11 @@ fun ActivitisPertama(modifier: Modifier) {
             ) {
             Row(){
                 val gambar = painterResource(id = R.drawable.logoumy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
+                )
             }
 
         }

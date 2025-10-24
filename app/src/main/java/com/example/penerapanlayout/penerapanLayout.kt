1bc131913629ba6c38247a484ad90bfc2f289051
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-24 16:50:30 +0700

    style: mengubah warna card menjadi biru

--- app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
+++ app/src/main/java/com/example/penerapanlayout/penerapanLayout.kt
@@ -72,9 +72,11 @@ fun ActivitisPertama(modifier: Modifier) {
         Spacer(modifier = Modifier.height(height = 25.dp))
         Card  (modifier = Modifier
             .fillMaxWidth(fraction = 1f)
-            .padding(all = 12.dp)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Blue
+                )
             ) {
-
         }
         Box(
             modifier = Modifier
